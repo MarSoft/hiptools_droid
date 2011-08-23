3d28dc3e0341ce9cb02b1bbcf920e8771a5092da
Семён Марьясин <marsoft@ya.ru>
2011-08-23 23:43:55 +0400

    Галерея. Первый блин: не показывает изображения.

--- src/org/marsoft/hiptools/HipToolsActivity.java
+++ src/org/marsoft/hiptools/HipToolsActivity.java
@@ -1,7 +1,17 @@
 package org.marsoft.hiptools;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class HipToolsActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +19,55 @@ public class HipToolsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Gallery g = (Gallery) findViewById(R.id.gallery1);
+        g.setAdapter(new ImageAdapter(this));
+        
+        g.setOnItemClickListener(new OnItemClickListener() {
+        	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+        		Toast.makeText(HipToolsActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+        	}
+		});
+    }
+    
+    public class ImageAdapter extends BaseAdapter {
+    	int mGalleryItemBackground;
+    	private Context mContext;
+    	
+    	private Integer[] mImageIds = {
+    			R.drawable.sample_1,
+    			R.drawable.sample_2
+    	};
+    	
+    	public ImageAdapter(Context c) {
+    		mContext = c;
+    		TypedArray a = obtainStyledAttributes(R.styleable.HipToolsGallery);
+    		mGalleryItemBackground = a.getResourceId(
+    				R.styleable.HipToolsGallery_android_galleryItemBackground, 0);
+    		a.recycle();
+    	}
+    	
+    	public int getCount() {
+    		return mImageIds.length;
+    	}
+    	
+    	public Object getItem(int position) {
+    		return mImageIds[position];
+    	}
+    	
+    	public long getItemId(int position) {
+    		return position;
+    	}
+    	
+    	public View getView(int position, View convertView, ViewGroup parent) {
+    		ImageView i = new ImageView(mContext);
+    		
+    		i.setImageResource(mImageIds[position]);
+    		i.setLayoutParams(new Gallery.LayoutParams(150, 100));
+    		i.setScaleType(ImageView.ScaleType.FIT_XY);
+    		i.setBackgroundResource(mGalleryItemBackground);
+    		
+    		return i;
+    	}
     }
 }
